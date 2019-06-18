4db2e75deb4d495691805876947fbcd23f1c7deb
Gregor Gorjanc <gregor.gorjanc@roslin.ed.ac.uk>
2019-06-18 15:30:03 +0000

    version 0.6.0

---
+++ inst/examples/examples_summary.AlphaPart.R
@@ -0,0 +1,31 @@
+## Load an example data
+data("AlphaPart.ped")
+
+## --- Partition additive genetic values by loc ---
+(res <- AlphaPart(x=AlphaPart.ped, colPath="loc", colAGV=c("agv1", "agv2")))
+
+## Summarize whole population
+(ret <- summary(res))
+
+## Summarize population by generation (=trend)
+(ret <- summary(res, by="gen"))
+
+## Summarize population by generation (=trend) and location 1
+(ret <- summary(res, by="loc.gen"))
+
+## Summarize population by generation (=trend) but only for location 1
+(ret <- summary(res, by="gen", subset=res[[1]]$loc == 1))
+
+## --- Partition additive genetic values by loc and sex ---
+
+AlphaPart.ped$loc.sex <- with(AlphaPart.ped, paste(loc, sex, sep="-"))
+(res <- AlphaPart(x=AlphaPart.ped, colPath="loc.sex", colAGV=c("agv1", "agv2")))
+
+## Summarize population by generation (=trend)
+(ret <- summary(res, by="gen"))
+
+## Summarize population by generation (=trend) and location 1
+(ret <- summary(res, by="loc.gen"))
+
+## Summarize population by generation (=trend) but only for location 1
+(ret <- summary(res, by="gen", subset=res[[1]]$loc == 1))
---
+++ inst/examples/examples_write.csv.R
@@ -0,0 +1,16 @@
+## Load an example data
+data("AlphaPart.ped")
+
+## Partition additive genetic values
+res <- AlphaPart(x=AlphaPart.ped, colPath="loc", colAGV=c("agv1", "agv2"))
+
+## Write summary on the disk and collect saved file names
+fileName <- file.path(tempdir(), "AlphaPart")
+ret <- write.csv(x=res, file=fileName)
+print(ret)
+file.show(ret[1])
+
+## Clean up
+files <- dir(path=tempdir(), pattern="AlphaPart*")
+unlink(x=files)
+
